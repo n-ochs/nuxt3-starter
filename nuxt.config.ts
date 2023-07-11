a90e2240f9c8577b06495d71df436f1baf734a9c
Nicholas Ochs <nick.ochs@yahoo.com>
2023-07-11 13:29:38 -0400

    upgrades packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/guide/concepts/rendering#hybrid-rendering
 export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: { typeCheck: true }
