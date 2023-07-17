30a138bf6e72ae6ecbe2ba2d33162c8158c3b6bd
Nicholas Ochs <nick.ochs@yahoo.com>
2023-07-17 09:51:36 -0400

    adds vue-query

---
+++ plugins/vue-query.ts
@@ -0,0 +1,28 @@
+import { useState } from '#app';
+import type { DehydratedState, VueQueryPluginOptions } from '@tanstack/vue-query';
+
+import { dehydrate, hydrate, QueryClient, VueQueryPlugin } from '@tanstack/vue-query';
+
+export default defineNuxtPlugin((nuxt) => {
+  const vueQueryState: Ref<DehydratedState | null> = useState<DehydratedState | null>('vue-query');
+
+  // Modify Vue Query global settings here
+  const queryClient: QueryClient = new QueryClient({
+    defaultOptions: { queries: { staleTime: 5000 } }
+  });
+  const options: VueQueryPluginOptions = { queryClient };
+
+  nuxt.vueApp.use(VueQueryPlugin, options);
+
+  if (process.server) {
+    nuxt.hooks.hook('app:rendered', () => {
+      vueQueryState.value = dehydrate(queryClient);
+    });
+  }
+
+  if (process.client) {
+    nuxt.hooks.hook('app:created', () => {
+      hydrate(queryClient, vueQueryState.value);
+    });
+  }
+});
